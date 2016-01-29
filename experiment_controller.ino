c3582fbe37ae7b58ce0f27b3b7b82b7823b2e1e1
Jonathan Card <joncard93@hotmail.com>
2016-01-29 17:26:31 -0500

    Initial outline.

---
+++ experiment_controller.ino
@@ -0,0 +1,15 @@
+// TODO: Turn on pin
+// TODO: Serial port speed
+
+void setup() {
+  // TODO: Set up turn on pin as OUTPUT, LOW
+  // TODO: Set up serial port
+  // TODO: Read time.
+  // TODO: Send the time so the host can synchronize RTC with milliseconds.
+}
+
+void loop() {
+  // TODO: Check sensors
+  // TODO: Format Sensor data to write out
+  // TODO: Listen for commands from the serial port
+}
